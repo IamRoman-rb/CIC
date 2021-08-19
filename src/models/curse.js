670697e7dd5d62fc5ac5350f70dc5f8ba10ce0cf
delficasarino <delficasarino@gmail.com>
2021-08-19 00:59:17 -0300

    course

--- src/models/curse.js
+++ src/models/curse.js
@@ -1,13 +1,29 @@
 const path = require("path");
 const fs = require("fs");
+const levelModel = require("./level");
 
-module.exports = {
-    directory: path.resolve(__dirname, "..data", "curses.json"),
-    all: () => {
+const model = {
+    directory: path.resolve(__dirname, "../data", "curses.json"),
+    all: function () {
         const file = fs.readFileSync(this.directory);
         return JSON.parse(file);
     },
-    one: (id) => {
-        return this.all().find(element => element.id == id);
+    allWidthExtra: function (id) {
+        return this.all().map(course => {
+            course.level = levelModel.one(course.level);
+            return course
+        })
+    },
+    one: function (id) {this.all().find(element => element.id == id)},
+    oneWidthExtra: function (id) {this.allWidthExtra().find(element => element.id == id)},
+    filter: function (prop,value) {
+        return this.allWidthExtra().filter(course => course[prop] == value)
+    }, 
+    search: function (prop,value) {
+        return this.allWidthExtra().find(course => course[prop] == value)
     }
 }
+
+console.log(model.all());
+
+module.export = model
