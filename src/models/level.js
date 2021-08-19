80c01d648608bbb955aa9ad8d20698adbcd9b4c6
EduardoMVirgilioDV <eduardo.virgilio@davinci.edu.ar>
2021-08-19 01:26:43 -0300

    Finalizado Modelo de Level

--- src/models/level.js
+++ src/models/level.js
@@ -4,16 +4,11 @@ const fs = require("fs");
 const modelLevel = {
     directory: path.resolve(__dirname, "../data", "levels.json"),
     levelStorage: path.resolve(__dirname, "../../public/updates/leves"),
-    all: () => {
-        const file = fs.readFileSync(modelLevel.directory);
-        return JSON.parse(file);
-    },
-    one: (id) => {
-        return modelLevel.all().find(element => element.id == id);
-    },
-    filter: (id) => modelLevel.all().filter(element => element.id == id),  //metodo search y exist
-    search: (id) => modelLevel.all().find(element => element.id == id),
-    exist: () => modelLevel.all().exist(element => element.id == id)
-    // checked: (id) => fs.existsSync(path.resolve(modelLevel.levelStorage, id)),
+    all: () => JSON.parse(fs.readFileSync(modelLevel.directory)),
+    one: id => modelLevel.all().find(element => element.id == id),
+    filter: (field,value) => modelLevel.all().filter(element => element[field] == value),  //metodo search y exist
+    search: (field,value) => modelLevel.all().find(element =>element[field] == value),
+    exist: (field,value) => modelLevel.search(field,value) != undefined,
 }
+
 module.exports = modelLevel
