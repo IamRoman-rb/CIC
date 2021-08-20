e7279b6f457af3626e00d56bcabcf84b142a3ad5
EduardoMVirgilioDV <eduardo.virgilio@davinci.edu.ar>
2021-08-19 22:35:32 -0300

    Actualizando Rutas y Controladores

--- src/routers/main.js
+++ src/routers/main.js
@@ -2,13 +2,9 @@ const path = require("path")
 const express = require("express");
 const app = express.Router();
 
-const controlador = require("../controllers/mainController");
+const controlador = require("../controllers/main");
 
 //Websites Routers
 app.get("/", controlador.index);
-app.get("/login", controlador.login);
-app.get("/register", controlador.register);
-app.get("/createFile", controlador.createFile);
-app.get("/createNews", controlador.createNews);
 
 module.exports = (app);
