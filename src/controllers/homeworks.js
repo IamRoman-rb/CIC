b07e52d1952aa8b7a0a4ad93ead15c5aa52bb7c5
EduardoMVirgilioDV <eduardo.virgilio@davinci.edu.ar>
2021-08-19 23:21:29 -0300

    Actualizando  Vistas con errores

--- src/controllers/homeworks.js
+++ src/controllers/homeworks.js
@@ -3,7 +3,7 @@ const controller = {
         return res.render("homeworks/index",{})
     },
     create: (req,res) => {
-        return res.render("homeworks/create",{})
+        return res.render("homeworks/create")
     },
     edit: (req,res) => {
         return res.render("homeworks/edit",{})
