9bfe4c203365948939772c1983d01d11b691fd0d
EduardoMVirgilioDV <eduardo.virgilio@davinci.edu.ar>
2021-08-29 13:40:52 -0300

    CRUD

--- src/middlewares/files.js
+++ src/middlewares/files.js
@@ -3,11 +3,14 @@ const path = require("path");
 const fs = require("fs");
 module.exports = (folder) => multer.diskStorage({
     destination:(req, file, cb) => {
-        let upload = path.join(__dirname,"../../public","upload",folder)
-        if(!fs.existsSync(upload)) {
-            fs.mkdirSync(upload)
+        let upload = path.resolve(__dirname,"../../public/uploads",folder)
+        console.log(upload);
+        if(fs.existsSync(upload)) {
+            return cb(null, upload)
+        } else {
+            fs.mkdirSync(upload);
+            return cb(null, upload)
         }
-        return cb(null, upload)
     },
     filename:(req, file, cb) => {
         return cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
