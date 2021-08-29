a680b1381645be0dc04643baccfbea5e422afc4c
IamRoman-rb <roman.borla2004@mail.com>
2021-08-29 17:24:59 -0300

    Arreglame esto Edu

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,8 @@
-let inputPassword = Array.from(document.querySelectorAll(".formulario-login-password"))
-let btnPassword = Array.from(document.querySelectorAll("#view-password"))
+let inputPassword = document.querySelectorAll(".formulario-login-password")
+let btnPassword = document.querySelector("#view-password")
 
-btnPassword.forEach((btn,index) => btn.addEventListener("click",e => {
+btnPassword.addEventListener("click",e => {
     e.preventDefault();
+    console.log("hola")
     inputPassword[index].getAttribute("type") == "password" ?  inputPassword[index].setAttribute("type","text") : inputPassword[index].setAttribute("type","password") 
-}))
+})
